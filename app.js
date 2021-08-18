38931b4745795f59562097b90fa8ac007dad6193
DanStevensonCO <danstevensonco@gmail.com>
2021-08-18 14:33:13 +0100

    install helmet

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const cors = require('cors')
+const helmet = require('helmet')
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(helmet())
 
 const apiRouter = require('./routes/api.js')
 
