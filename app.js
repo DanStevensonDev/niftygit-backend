9d6c2d4f9fe9df889562efe25e2e551715c90a52
DanStevensonCO <danstevensonco@gmail.com>
2021-08-19 09:02:53 +0100

    add header check and error handling

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
 const app = express()
+require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
 app.use(helmet())
+
+const { REACT_APP_HEADER_AUTH } = process.env
+
+app.use(function (req, res, next) {
+    const {authorization, host} = req.headers
+    if (authorization !== REACT_APP_HEADER_AUTH || !req.secure) {
+        return res.status(403).json({ error: 'Forbidden' });
+    }
+  next();
+})
 
 const apiRouter = require('./routes/api.js')
 
